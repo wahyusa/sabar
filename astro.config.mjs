23433611ca93d96ec372b9130eab30cba751416e
Wahyu Syamsul A'lam <wahyuskull22@gmail.com>
2023-01-29 10:59:38 +0000

    feat: :art: Force astro to run autoprefix in dev mode (unofficial)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+// import tailwind from "@astrojs/tailwind";
+import tailwind from "/lib/autoprefixdev";
 
 // https://astro.build/config
 export default defineConfig({
