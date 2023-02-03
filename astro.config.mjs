6501ceefeb673f5d175e2a31184a60bc0418110a
Wahyu Syamsul A'lam <wahyuskull22@gmail.com>
2023-02-03 03:48:55 +0000

    feat: :art: With these update, now I can use autoprefixer in development mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-// import tailwind from "@astrojs/tailwind";
-import tailwind from "/lib/autoprefixdev";
+import tailwind from "@astrojs/tailwind";
+// import tailwind from "/lib/autoprefixdev";
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
 });
