f7bfbc7116a95bb05c3454ada3ad992d96dc94d9
Wahyu Syamsul A'lam <wahyuskull22@gmail.com>
2023-06-12 14:40:35 +0000

    fix: Completely clean uninstall @astro/markdoc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
-import markdoc from "@astrojs/markdoc";
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [react(), markdoc()],
+  integrations: [react()],
   adapter: vercel()
 });
