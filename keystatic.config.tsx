08a674d11f0573c82d4aec1a531a57f48857b603
Wahyu Syamsul A'lam <wahyuskull22@gmail.com>
2023-06-12 13:42:43 +0000

    feat: Adding a collection data

--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -1,4 +1,4 @@
-import { config, fields, singleton } from '@keystatic/core'
+import { config, fields, singleton, collection } from '@keystatic/core'
 
 export default config({
   storage: {
@@ -14,6 +14,42 @@ export default config({
       path: 'src/content/_homepage',
       schema: {
         headline: fields.text({ label: 'Headline' }),
+      },
+    }),
+  },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      path : 'src/content/posts/*',
+      slugField: 'slug',
+      schema: {
+        title: fields.text({
+          label: 'Title',
+          validation: { length: { min: 4 } },
+        }),
+        slug: fields.text({
+          label: 'Slug',
+          validation: { length: { min: 4 } },
+        }),
+        publishedDate: fields.date({
+          label: 'Published Date',
+        }),
+        summary: fields.text({
+          label: 'Summary',
+          validation: { length: { min: 4, max: 120 } },
+        }),
+        content: fields.document({
+          label: 'Content',
+          formatting: true,
+          dividers: true,
+          links: true,
+          layouts: [
+            [1, 1],
+            [1, 1, 1],
+            [2, 1],
+            [1, 2, 1],
+          ],
+        }),
       },
     }),
   },
