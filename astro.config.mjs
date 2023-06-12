d1952fd9b1f97c6ebbc6c85271e1b0c584a1baa8
Wahyu Syamsul A'lam <wahyuskull22@gmail.com>
2023-06-12 14:30:45 +0000

    feat: Add @astro/markdoc integration
    This might be used by Document field from Keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [react()],
+  integrations: [react(), markdoc()],
   adapter: vercel()
 });
