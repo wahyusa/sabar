6985458747e18b78b26f47508a970e05dfb35083
Wahyu Syamsul A'lam <wahyuskull22@gmail.com>
2023-08-14 08:10:19 +0000

    feat: Add markdoc integration
    Including custom components and nodes for HTML tag

--- src/content/blog/cara-memanfaatkan-google-cloud-storage-google-cloud-sql-dan-bigquery-untuk-menyimpan-dan-menganalisis-data-besar.md
+++
@@ -1,3 +0,0 @@
-# Cara Memanfaatkan Google Cloud Storage, Google Cloud SQL, dan BigQuery untuk Menyimpan dan Menganalisis Data Besar
-
-Published on 30 Jul 2023
--- src/content/config.ts
+++ src/content/config.ts
@@ -7,7 +7,6 @@ const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     tags: z.array(z.string()),
-    image: z.string().optional(),
   }),
 });
 
