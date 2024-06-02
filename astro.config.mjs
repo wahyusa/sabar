a781761b83b87c000fb8d3ef052818b2a0f7f6b9
wahyusa <wahyuskull22@gmail.com>
2024-06-03 07:30:41 +0800

    feat: :poop: i18n following official docs works enough
    example.com/blog = default | example.com/en/blog = en |

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/static";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   output: "static",
-  adapter: vercel(),
+  adapter: vercel()
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,20 @@
+export const languages = {
+  id: "Bahasa Indonesia",
+  en: "English",
+};
+
+export const defaultLang = "id";
+
+export const ui = {
+  id: {
+    "nav.home": "Beranda",
+    "nav.about": "Tentang Saya",
+    "nav.twitter": "Twitter",
+  },
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+  },
+} as const;
+
+export const showDefaultLang = false;
